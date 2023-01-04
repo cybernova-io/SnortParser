bfeb7f8deda6c9fe6bfddd5a56369f4e196e086e
jrbrawner <jrbbrawner@gmail.com>
2023-01-03 20:07:41 -0600

    making progress

--- snort_parser.py
+++ snort_parser.py
@@ -15,6 +15,14 @@ class Parser:
     def parse_rule(self, input_string):
         self.lexer.input(input_string)
         while True:
+            result = self.parser.parse(input_string)
+            if not input_string:
+                break
+            print(result)
+
+    def lex_string(self, input_string):
+        self.lexer.input(input_string)
+        while True:
             tok = self.lexer.token()
             if not tok:
                 break
@@ -27,7 +35,7 @@ class Parser:
     'PORT',
     'DIRECTION',
     'ID',
-    'OPTIONS',
+    'OPTION',
     'LPAREN',
     'RPAREN',
     'COLON',
@@ -35,32 +43,36 @@ class Parser:
     'STRING_ESCAPE'
     ]
 
+
     reserved = {
-        'msg': 'MSG',
-        'logto': 'LOGTO',
-        'ttl': 'TTL',
-        'tos': 'TOS',
-        'ipoption': 'IPOPTION',
-        'fragbits': 'FRAGBITS',
-        'dsize': 'DSIZE',
-        'content': 'CONTENT',
-        'offset': 'OFFSET',
-        'depth': 'DEPTH',
-        'nocase': 'NOCASE',
-        'flags': 'FLAGS',
-        'seq': 'SEQ',
-        'ack': 'ACK',
-        'itype': 'ITYPE',
-        'icode': 'ICODE',
-        'session': 'SESSION',
-        'icmp_id': 'ICMP_ID',
-        'icmp_seq': 'ICMP_SEQ',
-        'ipoption': 'IPOPTION',
-        'rpc': 'RPC',
-        'resp': 'RESP',
-        'content-list': 'CONTENT_LIST',
-        'react': 'REACT'
+
     }
+    #reserved = {
+    #    'msg': 'MSG',
+    #    'logto': 'LOGTO',
+    #    'ttl': 'TTL',
+    #    'tos': 'TOS',
+    #    'ipoption': 'IPOPTION',
+    ##    'fragbits': 'FRAGBITS',
+    ##    'dsize': 'DSIZE',
+    #    'content': 'CONTENT',
+    #    'offset': 'OFFSET',
+    #    'depth': 'DEPTH',
+    #    'nocase': 'NOCASE',
+    #    'flags': 'FLAGS',
+    #    'seq': 'SEQ',
+    #    'ack': 'ACK',
+    #    'itype': 'ITYPE',
+    #    'icode': 'ICODE',
+    #    'session': 'SESSION',
+    #    'icmp_id': 'ICMP_ID',
+    #    'icmp_seq': 'ICMP_SEQ',
+    #    'ipoption': 'IPOPTION',
+    #    'rpc': 'RPC',
+    #    'resp': 'RESP',
+    #    'content_list': 'CONTENT_LIST',
+    #    'react': 'REACT'
+    #}
 
     tokens = tokens + list(reserved.values())
 
@@ -98,6 +110,10 @@ class Parser:
         r'(tcp|udp|icmp)'
         return t
 
+    def t_OPTION(self, t):
+        r'(msg|logto|ttl|tos|ipoption|fragbits|dsize|content|offset|depth|nocase|flags|seq|ack|itype|icode|session|icmp_id|icmp_seq|rpc|resp|content_list|react)'
+        return t
+
     def t_ID(self, t):
         r'[a-zA-Z_][a-zA-Z_0-9.]*' 
         t.type = self.reserved.get(t.value, 'ID')
@@ -109,13 +125,72 @@ class Parser:
     def p_rules(p):
         '''rules : rules rule
                  | rule'''
+        
+    @staticmethod
+    def p_rule(p):
+        '''rule : ACTION PROTOCOL IP PORT DIRECTION IP PORT LPAREN options RPAREN'''
+        
 
+    @staticmethod
+    def p_options(p):
+        '''options : options option
+                   | option'''
+        
+
+    @staticmethod
+    def p_option(p):
+        '''option :  OPTION COLON STRING_ESCAPE expression STRING_ESCAPE SEMICOLON'''
+        p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+        print('OPTION:', p[0])
+
+    @staticmethod
+    def p_expression(p):
+        '''expression : expression term
+                      | term'''
+        p[0] = p[1]
     
-    def p_rule(self, p):
-        '''rule : ACTION PROTOCOL IP PORT DIRECTION IP PORT LPAREN OPTIONS RPAREN'''
-        print(p[0])
+    @staticmethod
+    def p_term(p):
+        '''term : ID'''
+        p[0] = p[1]
+        print('TERM:', p[0])
 
-data = 'alert tcp 192.168.1.0 22 -> 192.168.1.1 80 (msg:"Test rule"; content:"Test content")'
-idk = Parser()
-idk.parse_rule(input_string=data)
+
+    #@staticmethod
+    #def p_term(p):
+    #    '''term : MSG
+    #            | LOGTO
+    #            | TTL
+    #            | TOS
+    #            | IPOPTION
+    #            | FRAGBITS
+    #            | DSIZE
+    #            | CONTENT
+    #            | OFFSET
+    #            | DEPTH
+    #            | NOCASE
+    #            | FLAGS
+    #            | SEQ
+    #            | ACK
+    #            | ITYPE
+    ###            | ICODE
+    #            | SESSION
+    #            | ICMP_ID
+    #            | ICMP_SEQ
+    #            | RPC
+    #            | RESP
+    #            | CONTENT_LIST
+    #            | REACT'''
+    #    
+    
+    # Error rule for syntax errors
+    def p_error(self, p):
+        message = 'Unknown text {} for token of type {} on line {}'.format(p.value, p.type, p.lineno)
+        raise TypeError(message, p.lineno, p.lexpos)
+
+#data = 'alert tcp 192.168.1.0 22 -> 192.168.1.1 80 (msg:"Test rule"; content:"Test content")'
+data = 'alert tcp 192.168.1.0 22 -> 192.168.1.1 80 (msg:"Test rule";)'
+parser = Parser()
+parser.parse_rule(input_string=data)
+#parser.lex_string(input_string=data)
 
